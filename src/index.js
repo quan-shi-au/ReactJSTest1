30519417a6e854335ebd99851ca9342ff83cc75d
Sam Quan <sam.quan@wontok.com>
2021-04-11 21:20:29 +1000

    react form

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import App from './App';
 import NewApp from './NewApp';
 import Game from './components/pages/Game';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+// ReactDOM.render(<Game />, document.getElementById('root'));
+
+const myfirstelement = <h1>Hello React!</h1>;
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
