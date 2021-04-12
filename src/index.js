ddf70b62f5aeeb0d476089fae9400a9104cedd63
Sam Quan <sam.quan@wontok.com>
2021-04-12 14:12:23 +1000

    add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import RouterApp from './RouterApp';
+import NestedRoutingApp from './NestedRoutingApp';
 import NewApp from './NewApp';
 import Game from './components/pages/Game';
 import Car from './components/pages/Car';
@@ -11,4 +13,8 @@ import Car from './components/pages/Car';
 
 // ReactDOM.render(myfirstelement, document.getElementById('root'));
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+// ReactDOM.render(<Car />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(<RouterApp />, document.getElementById('root'));
+// ReactDOM.render(<NestedRoutingApp />, document.getElementById('root'));
