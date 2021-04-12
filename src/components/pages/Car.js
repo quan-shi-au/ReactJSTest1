6d2ca5f7c88352617cedbc9e77d0048793b56401
Sam Quan <sam.quan@wontok.com>
2021-04-12 12:57:42 +1000

    add style

---
+++ src/components/pages/Car.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles/mysass.scss';
+
+class Car extends React.Component {
+    render() {
+        return <h1>Hello Car!</h1>
+    }
+}
+
+export default Car;
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import NewApp from './NewApp';
 import Game from './components/pages/Game';
+import Car from './components/pages/Car';
 
 // ReactDOM.render(<Game />, document.getElementById('root'));
 
-const myfirstelement = <h1>Hello React!</h1>;
+// const myfirstelement = <h1>Hello React!</h1>;
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+// ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+ReactDOM.render(<Car />, document.getElementById('root'));
